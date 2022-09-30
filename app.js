3291dabf7a85f2052a76fb38cb5827d03caaca3f
Osaretin Frank <osaretin.frank10@gmail.com>
2022-09-30 13:54:08 +0100

    changed socket FE domain

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const http = require("http")
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://master.d38cfclh6fsxhs.amplifyapp.com/",
   },
 })
 
